7189bfc23cd0b69823b3b783ec1006cfa298e7b8
JanBanasik <jan.jerzy.banasik@gmail.com>
2024-10-15 08:17:37 +0200

    Time: 125 ms (24.48%), Space: 18.5 MB (11.94%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        # problem is the same as move zeros
+        # just backwards
+        counter: int = 0
+        s = list(s)
+        left = len(s) - 1
+        for right in range(len(s) - 1, -1, -1):
+            if s[right] == "1":
+                s[left], s[right] = s[right], s[left]
+                counter += (left - right)
+                left -=1
+        return counter
