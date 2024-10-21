51f5581c8839ba890af98875120a0ad5a8d76c1c
JanBanasik <jan.jerzy.banasik@gmail.com>
2024-10-21 07:56:11 +0200

    Time: 152 ms (91.7%), Space: 16.6 MB (19.96%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,16 +1,19 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
-        return self.backtrack(s, 0, str(), set())
-    
-    def backtrack(self, s, currentIndex, currentSubstring, usedStrings):
-        if currentIndex == len(s) and not currentSubstring:
-            return len(usedStrings)
-        elif currentIndex == len(s):
-            return 0
-        
-        newUsed = usedStrings.copy()
-        currentSubstring += s[currentIndex]
-        newUsed.add(currentSubstring)
+        self.ans = 0
+        self.n = len(s)
+        self.backtrack(s, 0, set())
+        return self.ans
 
-        return max(self.backtrack(s, currentIndex + 1, currentSubstring, usedStrings),
-        self.backtrack(s, currentIndex + 1, str(), newUsed))
+    def backtrack(self, s, currentIndex, usedStrings):
+        if currentIndex == self.n:
+            self.ans = max(self.ans, len(usedStrings))
+        
+        for i in range(currentIndex, len(s)):
+            temp = s[currentIndex:i + 1]
+            if temp not in usedStrings:
+                usedStrings.add(temp)
+                self.backtrack(s, i + 1, usedStrings)
+                usedStrings.remove(temp)
+
+                
