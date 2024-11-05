20c0394a8096c5ddd56e039fe5c2b6214a836bea
JanBanasik <jan.jerzy.banasik@gmail.com>
2024-11-05 10:22:49 +0100

    Time: 19 ms (77.03%), Space: 17.1 MB (71.54%) - LeetHub

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minChanges(self, s: str) -> int:
+        moves = 0
+        for i in range(0, len(s) - 1, 2):
+            if s[i] != s[i+1]:
+                moves +=1
+        return moves
+
+ 
