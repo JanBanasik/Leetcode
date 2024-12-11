edc3dc39153a83e8e68084084dbfd277cda13ae2
JanBanasik <jan.jerzy.banasik@gmail.com>
2024-12-12 00:45:23 +0100

    Time: 99 ms (99.59%), Space: 28.3 MB (32.05%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,19 +1,10 @@
 class Solution:
-    def maximumBeauty(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        n = len(nums)
-        res = 0
-        for i in range(n):
-            l = 0
-            r = n - 1
-            t = 0
-            while l <= r:
-                mid = (r - l) // 2 + l
-                if nums[mid] - nums[i] <= 2 * k:
-                    t = mid
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            res = max(res, t - i + 1)
-        return res
-        
+  def maximumBeauty(self, nums: List[int], k: int) -> int:
+    nums.sort()
+    d = 2 * k
+    l = 0
+    for n in nums:
+      if nums[l] + d < n:
+        l += 1
+    return len(nums) - l
+
