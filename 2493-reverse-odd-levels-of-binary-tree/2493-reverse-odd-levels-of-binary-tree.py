7dcd570b546a1f93ac1a60ee611694e25c51ab2a
JanBanasik <jan.jerzy.banasik@gmail.com>
2024-12-20 13:28:52 +0100

    Time: 24 ms (51.17%), Space: 22.8 MB (5.99%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
@@ -6,24 +6,17 @@
 #         self.right = right
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        temp = []
-        self.getLevels(root, temp, 1)
-        temp = [temp[i][::-1] if i % 2 == 1 else temp[i] for i in range(len(temp))]
-        return self.constructBinaryTree(temp, 0, 0)
-    
-    def getLevels(self, root, temp, level):
         if root is None:
             return
-        if level > len(temp):
-            temp.append([])
-        temp[level - 1].append(root.val)
-        self.getLevels(root.left, temp, level + 1)
-        self.getLevels(root.right, temp, level + 1)
-    
-    def constructBinaryTree(self, temp, level, index):
-        if level >= len(temp):
-            return None
-        root = TreeNode(temp[level][index])
-        root.left = self.constructBinaryTree(temp, level + 1, 2 * index)
-        root.right = self.constructBinaryTree(temp, level + 1, 2 * index + 1)
+        self.traverse(root.left, root.right, 0)
         return root
+    
+    def traverse(self, left, right, level):
+        if left is None or right is None:
+            return
+        
+        if level % 2 == 0:
+            left.val, right.val = right.val, left.val
+        
+        self.traverse(left.left, right.right, level + 1)
+        self.traverse(left.right, right.left,  level + 1)
