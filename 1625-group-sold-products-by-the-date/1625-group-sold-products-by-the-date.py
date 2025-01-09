fdb009b3a7430f765f2671af1d9a3fc9a834fb86
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-01-09 10:02:47 +0100

    Time: 457 ms (57.47%), Space: 68.1 MB (37.85%) - LeetHub

--- 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.py
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
-def my_function(group):
-    p = [x for x in group['product']]
-    group['num_sold'] = len(p)
-    group['products'] = ",".join(sorted(p))
-    del group['product']
-    return group
 def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
-    activities.drop_duplicates(inplace=True)
-    newDf = activities.groupby('sell_date').apply(my_function)
-    newDf.drop_duplicates(inplace=True)
+    newDf = activities.groupby('sell_date').agg(
+        num_sold=('product', 'nunique'), 
+        products=('product', lambda x: ",".join(sorted(set(x))))).reset_index()
     return newDf
