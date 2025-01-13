02a301dde3de15a721598c40554c9b9a419ddc11
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-01-13 01:16:51 +0100

    Time: 234 ms (26.67%), Space: 18.7 MB (61.9%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        hashmap = defaultdict(int)
+        for c in s:
+            hashmap[c] +=1
+
+        result: int = 0
+        for key, value in hashmap.items():
+            if value >= 3:
+                if value % 2 == 1:
+                    result += 1 
+                else:
+                    result += 2
+            else:
+                result += value
+        return result
