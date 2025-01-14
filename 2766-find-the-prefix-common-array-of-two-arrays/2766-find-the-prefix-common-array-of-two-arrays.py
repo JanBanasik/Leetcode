2b060815108642b510bb25ffd45a2ed113f626f7
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-01-14 01:12:33 +0100

    Time: 15 ms (47.92%), Space: 18.1 MB (9.9%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
@@ -1,11 +1,18 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        cumulativeSetA = set()
-        cumulativeSetB = set()
-        result = []
-        for valueA, valueB in zip(A, B):
-            cumulativeSetA.add(valueA)
-            cumulativeSetB.add(valueB)
-            result.append(len(cumulativeSetA & cumulativeSetB))
+        setA = set()
+        setB = set()
+        result = [0] * len(A)
+        for index, (valueA, valueB) in enumerate(zip(A, B)):
+            result[index] = result[index - 1]
+            if valueA == valueB:
+                result[index] +=1
+            else:
+                if valueA in setB:
+                    result[index] +=1
+                if valueB in setA:
+                    result[index] +=1
+            setA.add(valueA)
+            setB.add(valueB)
             
         return result
