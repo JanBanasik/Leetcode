42cba41552c65c32434539c5eb826c946a8ffde8
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-02-12 01:14:17 +0100

    Time: 337 ms (57.16%), Space: 34.1 MB (8.34%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -7,7 +7,7 @@ class Node:
         newOrder = sorted([self.first, self.second, value])[-2:]
         self.second, self.first = newOrder
         
-    def result(self):
+    def calculateResult(self):
         return (self.first + self.second if self.second != -1 else -1)
     
     def __repr__(self):
@@ -20,7 +20,7 @@ class Solution:
             sumOfDigits = self.getSumOfDigits(num)
             h[sumOfDigits].append(num)
  
-        return max([v.result() for k, v in h.items()])
+        return max([v.calculateResult() for k, v in h.items()])
 
     def getSumOfDigits(self, number):
         result: int = 0
