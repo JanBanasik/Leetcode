dd7c0a0ba1d57c27bd918ecea6af0fbeed9d88af
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-02-12 01:13:54 +0100

    Time: 331 ms (58.81%), Space: 34 MB (8.34%) - LeetHub

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -0,0 +1,31 @@
+
+class Node:
+    def __init__(self):
+        self.first = -1 # the highiest 
+        self.second = -1 # second highiest
+    def append(self, value) -> None:
+        newOrder = sorted([self.first, self.second, value])[-2:]
+        self.second, self.first = newOrder
+        
+    def result(self):
+        return (self.first + self.second if self.second != -1 else -1)
+    
+    def __repr__(self):
+        return f"({self.first}, {self.second})"
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        h = defaultdict(Node)
+
+        for num in nums:           
+            sumOfDigits = self.getSumOfDigits(num)
+            h[sumOfDigits].append(num)
+ 
+        return max([v.result() for k, v in h.items()])
+
+    def getSumOfDigits(self, number):
+        result: int = 0
+        
+        while number > 0:
+            result += number % 10
+            number //= 10
+        return result
