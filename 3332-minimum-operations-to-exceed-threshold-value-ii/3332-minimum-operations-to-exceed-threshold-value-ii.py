bb1423e9045708bf90e6f38a7b4ca92242fdc8fb
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-02-13 01:05:10 +0100

    Time: 770 ms (5.04%), Space: 38.6 MB (12.18%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.py
@@ -2,11 +2,11 @@ from sortedcontainers import SortedList
 
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        s = SortedList(nums)
+        s = SortedList(nums, key = lambda x: -x)
         steps = 0
-        while len(nums) >= 2 and s[0] < k:
-            val1 = s.pop(0)
-            val2 = s.pop(0)
+        while len(nums) >= 2 and s[-1] < k:
+            val1 = s.pop(-1)
+            val2 = s.pop(-1)
             s.add(val1 * 2 + val2)
             steps +=1
         return steps
