6a39aad1a779405976870107228ef7cb0cd7411b
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-03-23 20:26:12 +0100

    Time: 287 ms (96.89%), Space: 67.1 MB (74.51%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.py
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
-    activity.sort_values(by = 'event_date', inplace=True)
-    activity.drop_duplicates(subset=['player_id'], keep='first', inplace=True)
-    result_df = activity[['player_id', 'event_date']]
-    result_df.columns = ['player_id', 'first_login']
-    return result_df
+    activity.sort_values(by = 'event_date', ascending = True, inplace=True)
+
+    result = activity.drop_duplicates(subset = ['player_id'], keep='first')[['player_id', 'event_date']]
+
+    columnNames = {'event_date': 'first_login'}
+    return result.rename(columns=columnNames)
