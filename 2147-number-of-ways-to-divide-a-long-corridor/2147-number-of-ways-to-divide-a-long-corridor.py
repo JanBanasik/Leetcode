9618610c9e459363a7c45f352721e07e16712f3d
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-12-14 18:18:52 +0100

    Time: 315 ms (44.76%), Space: 19.2 MB (58.1%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numberOfWays(self, corridor: str) -> int:
+        if corridor.count('S') % 2 == 1 or corridor.count('S') == 0:
+            return 0 
+        if corridor.count('S') == 2:
+            return 1
+        mod = 10**9 + 7
+        counts = []
+        counter = 0
+        plants = []
+        p = 0
+        while corridor[0] == 'P':
+            corridor = corridor[1::]
+        for i in corridor:
+            if i == 'S':
+                counter +=1
+                if counter == 1 and plants:
+                    plants.append(p+1)
+                    p = 0
+               
+                if counter == 4:
+                    plants.append(p + 1)
+                    p = 0
+                    counter = 0
+            else:
+                if counter == 2 or counter == 0:
+                    p +=1
+        res = 1
+        print(plants)
+        for i in plants:
+            res *= i
+        return res % mod
