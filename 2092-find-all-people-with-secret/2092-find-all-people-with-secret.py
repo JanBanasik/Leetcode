22a43075e6617df4910395ad7f613f462cbc71e8
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-12-19 01:09:16 +0100

    Time: 339 ms (48.41%), Space: 61 MB (68.79%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -2,6 +2,8 @@ class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
         meetings = sorted(meetings, key = lambda x: x[2])
         hashmap = {}
+        # hashmap[i] = x1: [y1,y2,y3] # czyli o czasie tym i tym spotyka sie jakas osoba i lista z kim sie spotyka
+        print(meetings)
         for p1, p2,time in meetings:
             if time not in hashmap:
                 hashmap[time] = []
@@ -14,14 +16,18 @@ class Solution:
                     break
             if not flag:
                 hashmap[time].append(set([p1,p2]))
+            
 
+        
         has_secret = set()
         has_secret.add(0)
         has_secret.add(firstPerson)
         for key, value in hashmap.items():
+            it = 1
+            found = False
             for x in range(len(value)):
                 for meeting in value:
                     if len(has_secret.intersection(meeting)) != 0:
                         for person in meeting:
                             has_secret.add(person)
-        return has_secret
+        return list(has_secret)
